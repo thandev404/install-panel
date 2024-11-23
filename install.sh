92ff4a08bbb6c4903d6f41bcb1f6ba69170525d4
thandev404 <stevennrald@gmail.com>
2024-11-23 21:38:03 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -56,7 +56,7 @@ install_jq() {
 check_token() {
   echo -e "                                                       "
   echo -e "${BLUE}[+] =============================================== [+]${NC}"
-  echo -e "${BLUE}[+]               LICENSY MASKHAN CODE            [+]${NC}"
+  echo -e "${BLUE}[+]               LICENSY MAS KHAN CODE            [+]${NC}"
   echo -e "${BLUE}[+] =============================================== [+]${NC}"
   echo -e "                                                       "
   echo -e "${YELLOW}MASUKAN AKSES TOKEN :${NC}"
